b237c9c57a4f1e8888eb1d406359cfe5fad3aa84
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-07 13:58:00 +0500

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <main>
       <h1>Pizza app website</h1>
+      <Button variant="outline">Button</Button>
     </main>
   );
 }

a8d0cc3405751e0c9312b525d1f0f153a68c2321
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-07 15:05:12 +0500

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
 
-
 export default function Home() {
   return (
     <main>
       <h1>Pizza app website</h1>
-      <Button variant="outline">Button</Button>
+      <Button>Button</Button>
     </main>
   );
 }

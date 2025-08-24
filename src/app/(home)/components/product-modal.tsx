001b5692a9430e49d79c8a540b790ac93a342676
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-24 20:08:16 +0500

    close modal on add to cart

--- src/app/(home)/components/product-modal.tsx
+++ src/app/(home)/components/product-modal.tsx
@@ -17,6 +17,7 @@ type ChosenConfig = {
 };
 
 const ProductModal = ({ product }: { product: Product }) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
   const cartItems = useAppSelector((state) => state.cart.cartItems);
   const dispatch = useAppDispatch();
 
@@ -101,6 +102,7 @@ const ProductModal = ({ product }: { product: Product }) => {
     };
 
     dispatch(addToCart(itemToAdd));
+    setDialogOpen(false)
   };
 
   const handleRadioChange = (key: string, data: string) => {
@@ -112,7 +114,7 @@ const ProductModal = ({ product }: { product: Product }) => {
   };
 
   return (
-    <Dialog>
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
       <DialogTrigger className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">
         Choose
       </DialogTrigger>

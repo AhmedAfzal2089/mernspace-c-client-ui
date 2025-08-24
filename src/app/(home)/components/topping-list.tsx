42a5388d06a7d23a630a7ec3a69a16ff0c3b8b9f
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-24 21:41:15 +0500

    fetch toppings dynamically by tenantId

--- src/app/(home)/components/topping-list.tsx
+++ src/app/(home)/components/topping-list.tsx
@@ -1,6 +1,7 @@
 import React, { startTransition, useEffect, useState } from "react";
 import ToppingCard from "./topping-card";
 import { Topping } from "@/lib";
+import { useSearchParams } from "next/navigation";
 
 const ToppingList = ({
   selectedToppings,
@@ -9,11 +10,14 @@ const ToppingList = ({
   selectedToppings: Topping[];
   handleCheckBoxCheck: (topping: Topping) => void;
 }) => {
+  const searchParams = useSearchParams();
   const [toppings, setToppings] = useState<Topping[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       const toppingResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/catalog/toppings?tenantId=9`
+        `${
+          process.env.NEXT_PUBLIC_BACKEND_URL
+        }/api/catalog/toppings?tenantId=${searchParams.get("restaurantId")}`
       );
 
       const json = await toppingResponse.json();

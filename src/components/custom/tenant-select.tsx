a45f9f2f84b702463efd5e5faff94b32c90ff2e6
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-24 21:36:25 +0500

    set the select restaurant default value on the refresh

--- src/components/custom/tenant-select.tsx
+++ src/components/custom/tenant-select.tsx
@@ -8,22 +8,27 @@ import {
   SelectValue,
 } from "../ui/select";
 import { Tenant } from "@/lib";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const TenantSelect = ({ restaurants }: { restaurants: { data: Tenant[] } }) => {
+  const searchParams = useSearchParams();
+
   const router = useRouter();
   const handleValueChange = (value: string) => {
     router.push(`/?restaurantId=${value}`);
   };
   return (
-    <Select onValueChange={handleValueChange}>
+    <Select
+      onValueChange={handleValueChange}
+      defaultValue={searchParams.get("restaurantId") || ""}
+    >
       <SelectTrigger className="w-[180px] focus:ring-0">
         <SelectValue placeholder="Select Restaurant" />
       </SelectTrigger>
       <SelectContent>
         {restaurants.data.map((restaurant) => {
           return (
-            <SelectItem key={restaurant.id} value={restaurant.id}>
+            <SelectItem key={restaurant.id} value={String(restaurant.id)}>
               {restaurant.name}
             </SelectItem>
           );

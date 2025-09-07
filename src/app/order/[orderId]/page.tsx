5a8bcb4ba6bb2fd507a472f784e63f7cbc7f1b30
AhmedAfzal2089 <ameafzal@gmail.com>
2025-09-08 00:35:56 +0500

    add customer id in field projection by default

--- src/app/order/[orderId]/page.tsx
+++ src/app/order/[orderId]/page.tsx
@@ -1,71 +1,93 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import React from 'react';
-import OrderStatus from './components/orderStatus';
-import { Separator } from '@/components/ui/separator';
-import { Banknote, Coins, LayoutDashboard } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React from "react";
+import OrderStatus from "./components/orderStatus";
+import { Separator } from "@/components/ui/separator";
+import { Banknote, Coins, LayoutDashboard } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cookies } from "next/headers";
 
-const SingleOrder = () => {
-    return (
-        <div className="container mt-6 flex flex-col gap-6">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Order</CardTitle>
-                    <CardDescription>Track the order status. </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <OrderStatus />
-                </CardContent>
-            </Card>
+const SingleOrder = async ({ params }: { params: { orderId: string } }) => {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/order/orders/${params.orderId}?fields=address,paymentStatus,paymentMethod`,
+    {
+      headers: {
+        Authorization: `Bearer ${cookies().get("accessToken")?.value}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch single order");
+  }
+  const order = await response.json();
+  console.log(order);
+  return (
+    <div className="container mt-6 flex flex-col gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Order</CardTitle>
+          <CardDescription>Track the order status. </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <OrderStatus />
+        </CardContent>
+      </Card>
 
-            <div className="flex gap-6">
-                <Card className="w-1/3">
-                    <CardHeader className="p-4">
-                        <CardTitle className="flex items-start text-lg justify-between gap-12">
-                            Delivery Address
-                        </CardTitle>
-                    </CardHeader>
-                    <Separator />
-                    <CardContent className="pt-6">
-                        <h2 className="font-bold">Rakesh K</h2>
-                        <p className="mt-2">55, New Street, upper lane, New Delhi. India. 409876</p>
-                    </CardContent>
-                </Card>
+      <div className="flex gap-6">
+        <Card className="w-1/3">
+          <CardHeader className="p-4">
+            <CardTitle className="flex items-start text-lg justify-between gap-12">
+              Delivery Address
+            </CardTitle>
+          </CardHeader>
+          <Separator />
+          <CardContent className="pt-6">
+            <h2 className="font-bold">Rakesh K</h2>
+            <p className="mt-2">
+              55, New Street, upper lane, New Delhi. India. 409876
+            </p>
+          </CardContent>
+        </Card>
 
-                <Card className="w-2/3">
-                    <CardHeader className="p-4">
-                        <CardTitle className="flex items-start text-lg justify-between gap-12">
-                            Your order information
-                        </CardTitle>
-                    </CardHeader>
-                    <Separator />
-                    <CardContent className="pt-6">
-                        <div className="flex items-center gap-2">
-                            <LayoutDashboard size={20} />
-                            <h2 className="text-base font-medium">Order reference: </h2>
-                            ord121313123131313
-                        </div>
-
-                        <div className="flex items-center gap-2 mt-2">
-                            <Banknote />
-                            <h2 className="text-base font-medium">Payment status: </h2>
-                            <span>Paid</span>
-                        </div>
-
-                        <div className="flex items-center gap-2 mt-2">
-                            <Coins size={20} />
-                            <h2 className="text-base font-medium">Payment method: </h2>
-                            <span>Card</span>
-                        </div>
-
-                        <Button variant={'destructive'} className="mt-6">
-                            Cancel Order
-                        </Button>
-                    </CardContent>
-                </Card>
+        <Card className="w-2/3">
+          <CardHeader className="p-4">
+            <CardTitle className="flex items-start text-lg justify-between gap-12">
+              Your order information
+            </CardTitle>
+          </CardHeader>
+          <Separator />
+          <CardContent className="pt-6">
+            <div className="flex items-center gap-2">
+              <LayoutDashboard size={20} />
+              <h2 className="text-base font-medium">Order reference: </h2>
+              ord121313123131313
             </div>
-        </div>
-    );
+
+            <div className="flex items-center gap-2 mt-2">
+              <Banknote />
+              <h2 className="text-base font-medium">Payment status: </h2>
+              <span>Paid</span>
+            </div>
+
+            <div className="flex items-center gap-2 mt-2">
+              <Coins size={20} />
+              <h2 className="text-base font-medium">Payment method: </h2>
+              <span>Card</span>
+            </div>
+
+            <Button variant={"destructive"} className="mt-6">
+              Cancel Order
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default SingleOrder;

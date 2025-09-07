c6e7faaa5cae5172160950e50883a892b3ac9081
AhmedAfzal2089 <ameafzal@gmail.com>
2025-09-07 16:02:26 +0500

    make success page dynamic

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -2,52 +2,94 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { ArrowLeft, CheckCircle2, LayoutDashboard, Store } from "lucide-react";
+import {
+  ArrowLeft,
+  CheckCircle2,
+  CircleX,
+  LayoutDashboard,
+  Store,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-const Payment = () => {
+const Payment = ({
+  searchParams,
+}: {
+  //   {/* // todo:get tenantId from redirect url, add it on backend */}
+
+  searchParams: { success: string; orderId: string };
+}) => {
+  const isOrderSuccess = searchParams.success === "true";
   return (
     <div className="flex flex-col items-center gap-4 w-full mt-12">
-      <CheckCircle2 size={80} className="text-green-500" />
-      <h1 className="text-2xl font-bold text-center">
-        Order Placed Successfully
-      </h1>
-      <p className="text-base font-semibold -mt-2">Thank you for your order.</p>
-      <Card className="w-1/3 mt-6">
-        <CardHeader>
-          <CardTitle className="flex items-start text-lg justify-between">
-            <div className="flex items-center gap-3">
-              <Store size={35} className="text-primary" />{" "}
-              <span>Your order information</span>
+      {isOrderSuccess ? (
+        <>
+          <CheckCircle2 size={80} className="text-green-500" />
+          <h1 className="text-2xl font-bold text-center">
+            Order Placed Successfully
+          </h1>
+          <p className="text-base font-semibold -mt-2">
+            Thank you for your order.
+          </p>
+        </>
+      ) : (
+        <>
+          <CircleX size={80} className="text-red-500" />
+          <h1 className="text-2xl font-bold text-center">
+            Payment has been Failed
+          </h1>
+          <p className="text-base font-semibold -mt-2">Please Try Again</p>
+        </>
+      )}
+      {isOrderSuccess && (
+        <Card className="w-1/3 mt-6">
+          <CardHeader>
+            <CardTitle className="flex items-start text-lg justify-between">
+              <div className="flex items-center gap-3">
+                <Store size={35} className="text-primary" />{" "}
+                <span>Your order information</span>
+              </div>
+              <Badge className="text-base px-4" variant={"secondary"}>
+                Confirmed
+              </Badge>
+            </CardTitle>
+          </CardHeader>
+          <Separator />
+          <CardContent className="pt-4">
+            <div className="flex items-center gap-2 ">
+              <LayoutDashboard size={20} />
+              <h2 className="text-base font-medium">Order reference:</h2>
+              <Link
+                href={`/order-status/${searchParams.orderId}`}
+                className="underline"
+              >
+                {searchParams.orderId}
+              </Link>
             </div>
-            <Badge className="text-base px-4" variant={"secondary"}>
-              Confirmed
-            </Badge>
-          </CardTitle>
-        </CardHeader>
-        <Separator />
-        <CardContent className="pt-4">
-          <div className="flex items-center gap-2 ">
-            <LayoutDashboard size={20} />
-            <h2 className="text-base font-medium">Order reference:</h2>
-            <Link href="/" className="underline">
-              23232323223
-            </Link>
-          </div>
-          <div className="flex items-center gap-2 mt-2">
-            <LayoutDashboard size={20} />
-            <h2 className="text-base font-medium">Payment Status:</h2>
-            <span>Paid</span>
-          </div>
-        </CardContent>
-      </Card>
-      <Button asChild className="mt-6">
-        <Link href="/" className="flex items-center gap-2">
-          <ArrowLeft size={20} className="text-white" />
-          <span>Place Another Order</span>
-        </Link>
-      </Button>
+            <div className="flex items-center gap-2 mt-2">
+              <LayoutDashboard size={20} />
+              <h2 className="text-base font-medium">Payment Status:</h2>
+              <span>Paid</span>
+            </div>
+          </CardContent>
+        </Card>
+      )}
+      {isOrderSuccess ? (
+        <Button asChild className="mt-6">
+          <Link href="/order/1223" className="flex items-center gap-2">
+            <ArrowLeft size={20} className="text-white" />
+            <span>Go to Order Status Page</span>
+          </Link>
+        </Button>
+      ) : (
+        <Button asChild className="mt-6">
+          {/* // todo:get tenantId from redirect url, add it on backend */}
+          <Link href="/checkout" className="flex items-center gap-2">
+            <ArrowLeft size={20} className="text-white" />
+            <span>Go to Checkout</span>
+          </Link>
+        </Button>
+      )}
     </div>
   );
 };
